a056a362087e0136376a25a7779c33a96e23adee
Alejandro Munoz <amunoz797@gmail.com>
2015-01-26 15:59:03 -0500

    Added Gyro Sensor

--- FTC/2014-2015 Cascade Effect!/Autonomous/rampAutoNoGoal.c
+++ FTC/2014-2015 Cascade Effect!/Autonomous/rampAutoNoGoal.c
@@ -1,45 +1,71 @@
-#pragma config(Hubs,   S1, HTMotor, HTServo, HTMotor, none)
-#pragma config(Motor,  mtr_S1_C1_1,     driveRight,   tmotorNormal, openLoop)
-#pragma config(Motor,  mtr_S1_C1_2,     lift,  tmotorNormal, openLoop)
-#pragma config(Servo,  srvo_S1_C2_1,    fieldGrabberRight, tServoStandard)
-#pragma config(Servo,  srvo_S1_C2_2,    fieldGrabberLeft, tServoStandard)
-#pragma config(Servo,  srvo_S1_C2_3,    scoopBridge, tServoStandard)
-#pragma config(Servo,  srvo_S1_C2_4,    frontBridge, tServoStandard)
-#pragma config(Motor,  mtr_S1_C3_1,     driveLeft,  tmotorNormal, openLoop)
-#pragma config(Motor,  mtr_S1_C3_2,     intake, tmotorNormal, openLoop)
+#pragma config(Hubs,   S1, HTMotor, HTServo, HTMotor, HTMotor)
+#pragma config(Motor,  mtr_S1_C1_1,     driveRight,    tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     lift,          tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_1,     driveLeft,     tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_2,     intake,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C4_1,     liftMotor3,    tmotorTetrix, openLoop)
+#pragma config(Servo,  srvo_S1_C2_1,    fieldGrabberRight,    tServoStandard)
+#pragma config(Servo,  srvo_S1_C2_2,    fieldGrabberLeft,     tServoStandard)
+#pragma config(Servo,  srvo_S1_C2_3,    scoopBridge,          tServoStandard)
+#pragma config(Servo,  srvo_S1_C2_4,    rampBridge,          tServoStandard)
 #pragma config(Sensor, S2,              sonarSensor,       sensorSONAR)
 #pragma config(Sensor, S3,     HTIRS2,              sensorI2CCustom)
+#pragma config(Sensor, S4,     HTGYRO,              sensorAnalogInactive)
 
 #include "JoystickDriver.c"
 #include "/hitechnic-irseeker-v2.h"
+#include "/hitechnic-gyro.h"
 
-#define open 0
-#define grab 100
-#define closed 105
-#define inside 215
+
+#define _open 0
+#define _closed 205
 #define _threshold 20
 
+
 int acS1, acS2, acS3, acS4, acS5 = 0;
+
+
+
+float exponentialJoystick(int joyVal){
+	return (float)5.60015*pow(2.718281828,0.96781*(abs(joyVal)/40));
+}
+
+void sticksDown(){
+	servo[fieldGrabberLeft] = _open;
+	servo[fieldGrabberRight] = 255-_open;
+}
+
+void sticksUp(){
+	servo[fieldGrabberLeft] = _closed-15;
+	servo[fieldGrabberRight] = 255-_closed;
+}
+
+void retainBalls()
+{
+	servo[scoopBridge] = 0;
+}
+
+void releaseBalls()
+{
+	servo[scoopBridge] = 180;
+}
+
+void closeRamp()
+{
+	servo[rampBridge] = 0;
+}
+
+void openRamp()
+{
+	servo[rampBridge] = 100;
+}
 
 void allStop(){
 	motor[driveLeft] = 0;
 	motor[driveRight] = 0;
 	motor[lift] = 0;
+	motor[liftMotor3] = 0;
 	motor[intake] = 0;
-	servo[frontBridge] = 127;
-	servo[scoopBridge] = 127;
-}
-
-void lowerBridge(){
-	servo[scoopBridge] = 256;
-}
-
-void raiseBridge(){
-	servo[scoopBridge] = 0;
-}
-
-void stopBridge(){
-	servo[scoopBridge] = 127;
 }
 
 void raiseLift(int powerLevel){
@@ -78,64 +104,69 @@ void right(int powerLevel){
 	motor[driveRight] = abs(powerLevel);
 }
 
-void rightStickDown(){
-		servo[fieldGrabberRight] = 255-inside;
-}
-
-void rightStickUp(){
-		servo[fieldGrabberRight] = 255-open;
-
-}
-
-void leftStickDown(){
-		servo[fieldGrabberLeft] = inside-15;
-}
-
-void leftStickUp(){
-		servo[fieldGrabberLeft] = open;
-}
-
-void sticksDown(){
-		servo[fieldGrabberLeft] = inside-15;
-		servo[fieldGrabberRight] = 255-inside;
-}
-
-void sticksUp(){
-		servo[fieldGrabberLeft] = open;
-		servo[fieldGrabberRight] = 255-open;
-}
-
 
 void init(){
-	sticksUp();
-	servo[scoopBridge] = 127;
-	servo[frontBridge] = 100;
+	servo[fieldGrabberLeft] = _open;
+	servo[fieldGrabberRight] = 255-_open;
+	servo[scoopBridge] = 180;
+	servo[rampBridge] = 0;
 	nMotorEncoder[intake] = 0;
+	openRamp();
+}
 
+void turn(int direction,int degrees,int powerLevel){//Dir:1 = right, 0 = left::Degrees
+
+  float rotSpeed = 0;
+  float heading = 0;
+  // Calibrate the gyro, make sure you hold the sensor still
+  HTGYROstartCal(HTGYRO);
+  while(true){
+	while (time1[T1] < 20)
+      wait1Msec(1);
+
+    // Reset the timer
+    time1[T1]=0;
+
+    // Read the current rotation speed
+    rotSpeed = HTGYROreadRot(HTGYRO);
+
+    // Calculate the new heading by adding the amount of degrees
+    // we've turned in the last 20ms
+    // If our current rate of rotation is 100 degrees/second,
+    // then we will have turned 100 * (20/1000) = 2 degrees since
+    // the last time we measured.
+    heading += rotSpeed * 0.02;
+    if(heading>degrees*(3.0/4.0))
+    	powerLevel=powerLevel/3;
+    if(direction==1)
+    	right(powerLevel);
+   	else if(direction==0)
+   		left(powerLevel);
+
+    if(abs(heading)>degrees)
+    	break;
+ }
 }
 
 task main()
 {
 	init();
-
-	//sticksDown();
-	//goto determineRotation;
-	servo[frontBridge] = 127;
 	waitForStart();
-	backward(20);
+	backward(100);
 	wait1Msec(1900); //Comes off ramp
 	allStop();
 	wait1Msec(500);
-	right(100);
-	wait1Msec(600);
+	/*turn(1,90,50);
 	allStop();
 	wait1Msec(700);
+	bFloatDuringInactiveMotorPWM = true;
 	forward(50);
-	wait1Msec(750);
+	wait1Msec(500);
+	bFloatDuringInactiveMotorPWM = false;
 	allStop();
-	wait1Msec(750);
+	wait1Msec(750);*/
 	//DO WHATEVER DEPENDING ON CENTER ROTATION
-	determineRotation:
+	/*determineRotation:
 	HTIRS2readAllACStrength(HTIRS2, acS1, acS2, acS3, acS4, acS5 );
 	int avg1=acS1,avg2,avg3,avg4,avg5;
 	for(int j=0;j<10;++j){
@@ -150,19 +181,19 @@ task main()
 		nxtDisplayCenteredTextLine(1,"Rot1");
 		forward(50);
 		wait1Msec(100);
-		left(100);
-		wait1Msec(600);
 		allStop();
 		wait1Msec(750);
+		turn(0,85,80);
+		allStop();
+		wait1Msec(750);
+		forward(50);
+		wait1Msec(950);
+		allStop();
+		wait1Msec(750);
+		turn(1,70,80);
+		allStop();
 		forward(100);
-		wait1Msec(600);
-		allStop();
-		wait1Msec(750);
-		right(100);
-		wait1Msec(550);
-		allStop();
-		forward(100);
-		wait1Msec(750);
+		wait1Msec(1250);
 		StopAllTasks();
 	}
 	else{
@@ -205,5 +236,5 @@ task main()
 		forward(100);
 		wait1Msec(750);
 	}
-}
+}*/
 }
